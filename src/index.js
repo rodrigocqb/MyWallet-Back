80a40d7278d0af347d376e73b8b0ce8ad784fb7c
rodrigocqb <rodrigocortibarros@gmail.com>
2022-09-11 20:48:26 -0300

    Separated database connection from index file

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
 import express from "express";
 import cors from "cors";
-import { MongoClient } from "mongodb";
 import joi from "joi";
 import { stripHtml } from "string-strip-html";
 import bcrypt from "bcrypt";
 import { v4 as uuid } from "uuid";
-import dotenv from "dotenv";
-dotenv.config();
+import db from "./database/db";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-const mongoClient = new MongoClient(process.env.MONGO_URI);
-let db;
-mongoClient.connect().then(() => {
-  db = mongoClient.db("my_wallet");
-});
 
 const signUpSchema = joi.object({
   name: joi.string().required(),
